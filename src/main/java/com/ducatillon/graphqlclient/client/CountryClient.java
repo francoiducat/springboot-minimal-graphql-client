7be8026e3771e00b4c093e8541b23bfe1b7f6825
francoiducat <francois.ducatillon@decathlon.com>
2021-06-18 15:22:55 +0200

    feedback : remove useless usage of IOUtils

--- src/main/java/com/ducatillon/graphqlclient/client/CountryClient.java
+++ src/main/java/com/ducatillon/graphqlclient/client/CountryClient.java
@@ -3,6 +3,7 @@ package com.ducatillon.graphqlclient.client;
 import com.ducatillon.graphqlclient.data.CountryDto;
 import com.ducatillon.graphqlclient.data.GraphqlRequestBody;
 import com.ducatillon.graphqlclient.util.GraphqlSchemaReaderUtil;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,14 +19,14 @@ public class CountryClient {
     this.url = url;
   }
 
-  public CountryDto getCountryDetails(final String countryCode) {
+  public CountryDto getCountryDetails(final String countryCode) throws IOException {
 
     WebClient webClient = WebClient.builder().build();
 
     GraphqlRequestBody graphQLRequestBody = new GraphqlRequestBody();
 
-    final String query = GraphqlSchemaReaderUtil.getSchemaFromFile("getCountryDetails");
-    final String variables = GraphqlSchemaReaderUtil.getSchemaFromFile("variables");
+    final String query = GraphqlSchemaReaderUtil.getSchemaFromFileName("getCountryDetails");
+    final String variables = GraphqlSchemaReaderUtil.getSchemaFromFileName("variables");
 
     graphQLRequestBody.setQuery(query);
     graphQLRequestBody.setVariables(variables.replace("countryCode", countryCode));
@@ -37,5 +38,4 @@ public class CountryClient {
         .bodyToMono(CountryDto.class)
         .block();
   }
-
 }
